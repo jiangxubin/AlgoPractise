2613da9c45196b8bcf1d2ffce4e026c8164009c8
jiangxubin <21612149@zju.edu.cn>
2018-06-15 17:41:40 +0800

    MaximumSubarray must be finished today

--- Fei.java
+++ Fei.java
@@ -13,8 +13,16 @@ class Fei{
         }
     }
 
+    public static int Solution2(int n){
+        if(n<1) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        else return Solution2(n-1) + Solution2(n-2);
+    }
+
     public static void main(String[] args) {
         HashMap<Integer, Integer> map = new HashMap<>();
         System.out.println(Solution(8, map));
+        System.out.println(Solution2(8));
     }
 }
--- MaximumSubarray.java
+++ MaximumSubarray.java
@@ -1,2 +1,9 @@
 class MaximumSubarray{
+    public static int Solution(int[] nums) {
+        return 1;       
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
